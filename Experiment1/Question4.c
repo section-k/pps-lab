ab3a4cb601ec90cf739f439212ae89fb08c78248
captain-Arvoy <accofstardust@gmail.com>
2022-11-13 19:02:04 +0530

    pushing all projects

---
+++ Experiment1/Question4.c
@@ -0,0 +1,13 @@
+/*		***Question 4***
+Write a program to input 3 integers into x,y,z Apply the following operations on them and analyse the result obtained
+i) r1= x>y>z
+ii)r2= x<y<z
+iii)r3 = x==y==z
+iv)r4 = x=y>z
+v)r5 = (x == y)&& (y==0)
+vi)r6 = x = x>>2
+vii) r7 = !(x=0)
+*/
+#include <stdio.h>
+int main(){
+	int x, y, z;
