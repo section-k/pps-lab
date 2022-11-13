1934770f4657864346e8b88a0cd97d5753f6b427
captain-Arvoy <accofstardust@gmail.com>
2022-11-13 13:57:25 +0530

    Question 2 done

---
+++ Experiment1/Question2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+	float phy_mark, chem_mark, maths_mark, percent;
+	printf ("Enter marks of Physics: " );
+	scanf("%f",&phy_mark);
+	printf("Enter marks of Chemistry: ");
+	scanf("%f",&chem_mark);
+	printf("Enter marks of Mathematics: ");
+	scanf("%f",&maths_mark);
+	percent = (phy_mark + chem_mark + maths_mark)/120;
+	percent = percent * 100;
+	printf("The percentage obtained by student = %f",percent);
+	return 0;
+}
+/* 
+	O/p
+Enter marks of Physics: 34
+Enter marks of Chemistry: 27
+Enter marks of Mathematics: 35
+The percentage obtained by student = 80.000000
+ */
+
