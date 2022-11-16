336724cdcf27818de7401e1a49b2434ccbe96aa0
captain-Arvoy <accofstardust@gmail.com>
2022-11-16 10:24:18 +0530

    Removed exe files

--- Experiment4/Question4.c
+++ Experiment4/Question4.c
@@ -10,4 +10,19 @@ vii) r7 = !(x=0)
 */
 #include <stdio.h>
 int main(){
-	int x, y, z;
+	int x, y, z, r1,r2, r3, r4,r5, r6, r7;
+	printf("Enter three unknown numbers: ");
+	scanf("%d%d%d",&x,&y,&z);
+	r1 = x > y > z;
+	r2 = x < y <= z;
+	r3 = x== y == z;
+	r4 = (x = y >z);
+	r5 = (x == y) && (y == 0);
+	r6 = x = (x >> 2);
+	r7 = !(x=0);
+	printf("%d%d%d%d%d%d%d",r1,r2,r3,r4,r5,r6,r7);
+	return 0;
+}
+/* o/p
+Enter three unnknown nos 12 23 45
+*/
