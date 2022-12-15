6e24ad523c30a0f64f76fb3a77eee142ad2dd02b
captain-Arvoy <accofstardust@gmail.com>
2022-12-15 21:29:14 +0530

    Updated

--- Experiment8/Question1.c
+++ Experiment8/Question1.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-int checkprime(int n){
-	int i = 2;
-	do{
-		if (n % i == 0){
-			return 0;
-		}
-		i++;
-	}while(i <= sqrt(n));	
-	return 1;
-}
 int main(){
-	int number;
+	int number, i = 2, flag = 1;
 	printf("Enter the number: ");
 	scanf("%d",&number);
-	if (checkprime(number)){
+	do{
+		if (number % i == 0){
+			flag = 0;
+		}
+		i++;
+	}while(i <= sqrt(number));
+	if (flag){
 			printf("prime");
 	} else {
 		printf("Not prime");
