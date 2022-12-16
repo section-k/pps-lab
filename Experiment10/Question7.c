7e4cdddf22d50716cebe5302d4384413b8fd8f53
captain-Arvoy <accofstardust@gmail.com>
2022-12-16 08:23:22 +0530

    Experiment 10 done

---
+++ Experiment10/Question7.c
@@ -0,0 +1,27 @@
+/*					***Question 7***
+ Write a program to input a string. Sort the strin gin alphabetical order and display it.
+ */
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char inputString[30];
+	printf("Enter the string: ");
+	scanf("%[^\n]s",inputString);
+	
+	printf("%s\n",inputString);
+	int length = strlen(inputString);
+	//Bubble sort Ascending
+	for (int i = 0; i < length; i++){
+		for (int j = 0; j < length - i - 1; j++){
+			if ( inputString[j + 1] < inputString[j]){
+				char temp = inputString[j];
+				inputString[j] = inputString[i];
+				inputString[i] = temp;
+			}
+		}
+	}
+	printf("\n%s\n",inputString);
+	return 0;
+}
+
+
